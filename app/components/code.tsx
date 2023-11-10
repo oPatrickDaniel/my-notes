997efbd3857be1832156a5681083f73ad4911abe
Patrick Daniel <contato.pdani@gmail.com>
2023-11-10 14:54:31 -0300

    feat: remove tab modifiers

--- app/components/code.tsx
+++ app/components/code.tsx
@@ -1,4 +1,3 @@
-
 'use client'
 import { useState } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
@@ -9,59 +8,54 @@ import Checkmark from './checkmark'
 interface codeCmpProps {
   language: string
   children: string
-  noTabs?: number
 }
 
 export default function CodeCmp(props: codeCmpProps) {
 
-  function removeTabs(str: string, numTabs: number): string {
-    const lines = str.split('\n');
-    const linesWithoutTabs = lines.map(line => line.replace(new RegExp(`^\\s{0,${numTabs}}`), ''));
-    return linesWithoutTabs.join('\n');
-  }
-
-  const codeString = removeTabs(props.children, props.noTabs ?? 14)
+  const codeString = props.children
   const [copy, setCopy] = useState(false)
 
   return (
     <>
-      <div className='mt-3 rounded overflow-hidden'>
-        <div className='flex justify-between px-4 bg-gray-700 text-white text-xs items-center '>
+      <div className="mt-3 rounded overflow-hidden">
+        <div className="flex justify-between px-4 bg-gray-700 text-white text-xs items-center ">
           <p className="text-sm">Exemplo</p>
           {copy ? (
-            <button className='py-2 flex justify-center items-center gap-1'>
-              <span className='w-4'>
+            <button className="py-2 flex justify-center items-center gap-1">
+              <span className="w-4">
                 <Checkmark />
               </span>
               COPIADO!
             </button>
           ) : (
-            <button className='py-2 flex justify-center items-center gap-1' onClick={() => {
-              navigator.clipboard.writeText(codeString)
-              setCopy(true)
-              setTimeout(() => {
-                setCopy(false)
-              }, 1000);
-            }}
+            <button
+              className="py-2 flex justify-center items-center gap-1"
+              onClick={() => {
+                navigator.clipboard.writeText(codeString)
+                setCopy(true)
+                setTimeout(() => {
+                  setCopy(false)
+                }, 1000)
+              }}
             >
-              <span className='w-4'>
+              <span className="w-4">
                 <ClipboardOutlineCmp />
               </span>
               COPIAR
-            </button>)
-          }
+            </button>
+          )}
         </div>
         <SyntaxHighlighter
           language={props.language}
           style={stackoverflowDark}
           customStyle={{
-            padding: '10px'
+            padding: '10px',
           }}
           wrapLongLines={true}
         >
           {codeString}
         </SyntaxHighlighter>
-      </div >
+      </div>
     </>
   )
 }
