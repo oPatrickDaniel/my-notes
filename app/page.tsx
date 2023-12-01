f48c73078f7c70660ca9f5d2b86767db6ba56eda
Patrick Daniel <contato.pdani@gmail.com>
2023-11-30 23:52:18 -0300

    feat: add well, cleanCode, solid, poo notes

--- app/page.tsx
+++ app/page.tsx
@@ -88,6 +88,120 @@ valor-hora = salario-mensal / horas-trabalhadas-por-mes
 print valor-hora`}
         </CodeCmp>
       </SectionCmp>
+
+      <SectionCmp title="clean code">
+        <p>
+          Os princípios do clean code foram popularizados por Robert C. Martin
+          em seu livro &quot;Clean Code: A Handbook of Agile Software
+          Craftsmanship&quot;. Esses princípios tem como objetivo criar um
+          código legível e compreensivo a humanos, tornando fácil sua manutenção
+        </p>
+        <h3></h3>
+      </SectionCmp>
+
+      <SectionCmp title="P.O.O">
+        <p>
+          Poo e um acrônimo para programação orientada a objetos. Essa é o
+          paradigma de programação mais usado atualmente. O poo usa como pilar
+          quatro pilares, sendo esses herança, polimorfismo, encapsulamento e
+          abstração.
+        </p>
+        <h3>Herança</h3>
+        <p>
+          A herança consiste em reaproveitar trechos de código na crianção de um
+          novo semelhante. Por exemplo uma class carro que possui as
+          propriedades nome e marcar e motor poderiam ser estendidas por uma
+          nova chamada carroElétrico que além das propriedades de carro poderia
+          ter a prioridade bateria
+        </p>
+        <h3>Polimorfismo</h3>
+        <p>
+          O polimorfismo consiste em identificar objetos de duas classes
+          diferentes que tem o mesmo método implementado de formas diferentes.
+          Nestes casos o objeto pode ser modificado na classe herdeira
+          resultando no mesmo resultado porem de formas diferentes.
+        </p>
+        <h3>Encapsulamento</h3>
+        <p>
+          O encapsulamento consiste em tornar dados sensíveis inacessíveis fora
+          da classe, usando private por exemplo.
+        </p>
+      </SectionCmp>
+
+      <SectionCmp title="Design Patterns">
+        <p>
+          Os patters são soluções reutilizáveis para problemas comuns no
+          desenvolvimento. Criados pelos Gof(gang of for) eles são padrões
+          documentados em 23 formas e divididas em 3 categorias: creational,
+          structural, behavioral
+        </p>
+        <h3>Creational Patterns</h3>
+        <p>
+          Esses padrões lidam com o processo de criação de objetos. Eles
+          fornecem mecanismos para instanciar objetos de maneira a tornar um
+          sistema independente de sua criação, composição e representação.
+        </p>
+        <h3>Structural Patterns</h3>
+        <p>
+          Esses padrões concentram-se na composição de objetos para formar
+          estruturas maiores. Eles permitem definir como classes e objetos podem
+          ser combinados para formar grandes estruturas, mantendo ao mesmo tempo
+          flexibilidade e reutilização.
+        </p>
+        <h3>Behavioral Patterns</h3>
+        <p>
+          Esses padrões estão relacionados à forma como objetos interagem e se
+          comunicam entre si. Eles definem modelos de comunicação entre objetos
+          para facilitar a colaboração e a responsabilidade das tarefas.
+        </p>
+      </SectionCmp>
+
+      <SectionCmp title="S.O.L.I.D">
+        <p>
+          Solid é um acrônimo de letras, cada letra representando um principio
+          de designe de software que visa cria um código compreensível e
+          sustentável
+        </p>
+        <h3>Single Responsibility Principle - SRP</h3>
+        <p>
+          O principio da responsabilidade unica diz que cada objeto deve ter
+          apenas uma responsabilidade, e uma razão para mudar. Este conceito
+          facilita possíveis mudanças no código.
+        </p>
+        <h3>Open/Closed Principle - OCP</h3>
+        <p>
+          O principio do aberto fechado diz que uma classe deve estar aberta
+          para extensão, mas fechada para modificação. Seguindo este conceito
+          novas features devem ser adicionadas no código via extensão sem
+          modificar a classe original o que previne bugs na parte do código já
+          operacional.
+        </p>
+        <p></p>
+        <h3>Liskov Substitution Principle - LSP</h3>
+        <p>
+          O principio da substituição de Liskov diz que que classe derivada deve
+          ser capaz de substituir os Objetos da classe base sem afetar a
+          integridade do programa. Isso quer dizer que deve haver o cuidado ao
+          extender uma classe para que as substituições na classe derivada não
+          crie bugs que não existiam na classe original.
+        </p>
+        <h3>Interface Segregation Principle - ISP</h3>
+        <p>
+          O principio da segregação de interfaces diz que uma classe não deve
+          ser forçada a implementar interfaces que não utiliza. Na pratica isso
+          significa que interfaces devem ser menores e especificas, evitando a
+          obrigação de implementar métodos desnecessários.
+        </p>
+        <h3>Dependency Inversion Principle - DIP</h3>
+        <p>
+          O Principio da inversão de dependências diz que devemos depender de
+          abstrações e não de implementações e módulos de alto nível não devem
+          depender de módulos de baixo nível. Ambos devem depender de
+          abstrações. Na pratica isso quer dizer que caso haja um detalhe no
+          código como um banco de dados, o funcionamento da classe não pode
+          depender desse detalhe, e este deve ser injetado no código principal.
+        </p>
+      </SectionCmp>
     </>
   )
 }
